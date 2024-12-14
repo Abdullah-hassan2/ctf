99d81bb5f32a4b7f619c59bfe9d6ae170ca7e4d8
Abdullah hassan <151984495+Abdullah-hassan2@users.noreply.github.com>
2024-12-14 13:45:41 +0500

    Update checkFlag.js

--- functions/checkFlag.js
+++ functions/checkFlag.js
@@ -1,6 +1,5 @@
-// For Vercel, use module.exports instead of exports.handler
 module.exports = async (req, res) => {
-  // Enable CORS
+  // CORS Headers
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
@@ -9,20 +8,15 @@ module.exports = async (req, res) => {
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   );
 
-  // Handle OPTIONS request
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
   }
 
-  // Only allow POST requests
   if (req.method === 'POST') {
     const { flag } = req.body;
-    
-    // Define the correct flag
     const correctFlag = 'robot{elliot_alderson}';
   
-    // Check if the submitted flag is correct
     if (flag === correctFlag) {
       res.status(200).json({ 
         message: 'Congratulations! You have solved this challenge!' 
@@ -33,6 +27,6 @@ module.exports = async (req, res) => {
       });
     }
   } else {
-    res.status(405).end(); // Method Not Allowed
+    res.status(405).end();
   }
 };
